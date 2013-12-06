223168a76323411d146942d603cae8b33d03f430
Hydrations <maxholzmann@gmail.com>
2013-12-05 21:47:16 -0600

    BukkitAPI - Episode #3

---
+++ src/me/hydrations/First_Cmd/Main.java
@@ -0,0 +1,29 @@
+package me.hydrations.First_Cmd;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("maxtheorange")) { //if the entered command is "maxtheorange"
+			if(sender instanceof Player) { // if the command sender is a player
+				Player p = (Player)  sender; //define the variable of the sender 
+				p.sendMessage(ChatColor.GREEN + "This is my first command! Woot!");
+			}
+		}
+		return true;
+	}
+	
+	public void onDisable() {
+		
+	}
+}
+
